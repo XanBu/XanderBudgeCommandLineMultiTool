d072912d2bfc5a20e50e69562094dbfd6d283b74
Xander B <dylanxan05@gmail.com>
2026-01-20 20:48:24 -0500

    MultiTool.py created

---
+++ MultiTool.py
@@ -0,0 +1,22 @@
+import subprocess #used to execute other commands
+import sys #for executable
+
+def main():
+    #title
+    print("=== Multitool ===")
+    print("1) Calendar")
+    print("q) Quit")
+
+    #get user input
+    choice = input("> ").strip().lower()
+
+    if choice == "1": # calcure calander *must be previously installed by user*
+        subprocess.run([sys.executable, "-m", "calcure"])
+    elif choice == "q": #exit
+        return
+    else:
+        print("Unknown option") #other catch-all
+
+
+if __name__ == "__main__":
+    main()
